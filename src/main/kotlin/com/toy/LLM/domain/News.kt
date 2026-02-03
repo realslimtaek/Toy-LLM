9d618e283d6d93feeea5b4643514fcd650eb7552
realslimtaek <xorgud777@gmail.com>
2026-02-03 15:43:11 +0900

    remove: 사용하지 않을 파일 제거

--- src/main/kotlin/com/toy/LLM/domain/News.kt
+++
@@ -1,4 +0,0 @@
-package com.toy.LLM.domain
-
-class News {
-}
--- src/main/kotlin/com/toy/LLM/infrastructure/persistence/news/repository/NewsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.toy.LLM.infrastructure.persistence.news.repository
-
-import com.toy.LLM.infrastructure.persistence.news.entity.NewsEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface NewsRepository : JpaRepository<NewsEntity, Long> {
-}
